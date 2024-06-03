066a0f1e0262e041d0f6c50ee845b6e9c8ecfe88
Laura <laura.e.vollmer@gmail.com>
2024-06-03 16:51:24 -0500

    setting up the Home page

---
+++ src/assets/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+// import Header from './assets/Header.jsx'
+
+const Home = () => {
+  return (
+    <div>
+      <div>
+        <h1>Manage Your Art Business</h1>
+        <p>Please help me manage it.</p>
+      </div>
+    </div>
+  )
+}
+
+export default Home
