227b632d9f42ca71b233d446b79128bfb59d56ce
Laura <laura.e.vollmer@gmail.com>
2024-06-04 00:04:17 -0500

    added support component

---
+++ src/assets/Support.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Support = () => {
+  return (
+    <div>
+        {/* FAQs */}
+        <h1>Support</h1>
+        <p>This is some paragraph text.</p>
+    </div>
+  )
+}
+
+export default Support
