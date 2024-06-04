617a022cfbb48f8458403ff5665dfff37fdec172
Laura <laura.e.vollmer@gmail.com>
2024-06-04 00:06:15 -0500

    added a schedule component

---
+++ src/assets/Schedule.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Schedule = () => {
+  return (
+    <div>
+      <TextBlock
+        title="Plan your Art Schedule"
+        text="This is some paragraph text."
+      />
+      <TextBlock
+        title="Event Booking Form"
+        text="This is some paragraph text."
+      />
+      <TextBlock title="Event Calendar" text="This is some paragraph text." />
+    </div>
+  );
+};
+
+export default Schedule;
