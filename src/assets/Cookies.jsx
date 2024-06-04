6580f90b3b540ab5dbab0d497895514233787115
Laura <laura.e.vollmer@gmail.com>
2024-06-04 00:11:16 -0500

    added cookies and 404 error page

---
+++ src/assets/Cookies.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Cookies = () => {
+  return (
+    <div>
+      <TextBlock title="Cookies Policy" text="This is some paragraph text." />
+    </div>
+  );
+};
+
+export default Cookies;
---
+++ src/assets/Error.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Error = () => {
+  return (
+    <div> <TextBlock title="Page Not Found" text="This is some paragraph text." /></div>
+  )
+}
+
+export default Error
