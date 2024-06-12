07c02d4cd638b91fcd5385d159ddcfc5bd7ca904
Laura <laura.e.vollmer@gmail.com>
2024-06-11 21:38:47 -0500

    updating blog component

--- src/assets/Blog.jsx
+++ src/assets/Blog.jsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <div>
+    <TextImageBlock
+      title="Latest Posts"
+      text="This is some paragraph text."
+      imageUrl="/path/to/image.jpg"
+    />
+    <TextImageBlock
+      title="Categories"
+      text="This is some paragraph text."
+      imageUrl="/path/to/image.jpg"
+    />
+    <DemoBlock
+      title="Subscribe to the Blog"
+      text="This is some paragraph text."
+      imageUrl="/path/to/image.jpg"
+      buttonText="Start Demo"
+    />
+    <TextBlock title="Join the Newsletter" text="Click below to join the newsletter to learn more." />
+  </div>
   )
 }
 
